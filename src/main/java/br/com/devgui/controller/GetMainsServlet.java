3ec99ad309fee0ba0cc535d508e108d23c313be4
Guilherme Rivaldy <guilherme.rivaldy@upe.br>
2025-03-23 17:57:11 -0300

    feat: add logic to doGet method int GetMainsServlet

--- src/main/java/br/com/devgui/controller/GetMainsServlet.java
+++ src/main/java/br/com/devgui/controller/GetMainsServlet.java
@@ -1,6 +1,17 @@
 package br.com.devgui.controller;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import com.google.gson.Gson;
+
+import br.com.devgui.controller.response.ChampionResponseDTO;
+import br.com.devgui.controller.response.MainsDetailsResponseDTO;
+import br.com.devgui.model.Champion;
+import br.com.devgui.model.Player;
+import br.com.devgui.service.DataDragonApiService;
+import br.com.devgui.service.RiotApiService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,9 +22,52 @@ import jakarta.servlet.http.HttpServletResponse;
 public class GetMainsServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    private final RiotApiService riotApiService = new RiotApiService();
+    private final DataDragonApiService dataDragonApiService = new DataDragonApiService();
+    private final Gson gson = new Gson();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	super.doGet(req, resp);
+    	Optional<String[]> params = getParams(req.getPathInfo());
+    	
+    	if(params.isEmpty()) {
+    		throw new RuntimeException("ta vazio os params.");
+    	}
+    	
+    	String gameName = params.get()[0];
+    	String tagLine = params.get()[1];
+    	
+    	Player player = riotApiService.getPlayer(gameName, tagLine);
+    	List<Champion> champions = riotApiService.getChampionMasteryId(player.getPuuid());
+    	
+    	List<Champion> championsCompleted = dataDragonApiService.getChampionDetails(champions);
+    	
+    	List<ChampionResponseDTO> championsDTO = championsCompleted.stream()
+    			.map(c -> new ChampionResponseDTO(
+    					c.getChampionName(), 
+    					c.getTitle(), 
+    					c.getMasteryPoints()
+    					))
+    			.toList();
+    	
+    	MainsDetailsResponseDTO detailsResponseDTO = new MainsDetailsResponseDTO(
+    			player.getGameName(),
+    			player.getTagLine(), 
+    			championsDTO);
+    	
+    	String responseJson = gson.toJson(detailsResponseDTO);
+    	resp.getWriter().append(responseJson);
+    }
+    
+    private static Optional<String[]> getParams(String pathInfo) {
+    	if (pathInfo != null) {
+    		String[] params = pathInfo.substring(1).split("/");
+    		
+    		if (params.length == 2) {
+    			return Optional.of(params);
+    		}
+    		return null;
+    	}
+    	return null;
     }
 }
