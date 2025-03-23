631cda241d181b98c3a16e21ca92649027d09a5a
Guilherme Rivaldy <guilherme.rivaldy@upe.br>
2025-03-23 15:51:55 -0300

    feat: add methods to create and send requests

--- src/main/java/br/com/devgui/service/RiotApiService.java
+++ src/main/java/br/com/devgui/service/RiotApiService.java
@@ -1,5 +1,31 @@
 package br.com.devgui.service;
 
-public class RiotApiService {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+import com.google.gson.Gson;
+
+public class RiotApiService {
+	
+	private final String URL_ACCOUNT = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/";
+	private final String URL_CHAMPION_MASTERY = "https://br1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/";
+	
+	private final String API_KEY = "RGAPI-381b0028-06f7-4653-a427-28471abac35c";
+	
+	private final Gson gson = new Gson();
+	private final HttpClient client = HttpClient.newHttpClient();
+	
+	private HttpRequest createRequest(String url) {
+		return HttpRequest.newBuilder()
+				.uri(URI.create(url))
+				.GET()
+				.build();
+	}
+	
+	private HttpResponse<String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
+		return client.send(request, HttpResponse.BodyHandlers.ofString());
+	}
 }
