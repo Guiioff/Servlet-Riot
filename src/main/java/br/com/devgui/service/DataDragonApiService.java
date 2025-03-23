50cdb46ef58daf77c6c7db45db27375b6dbde3c4
Guilherme Rivaldy <guilherme.rivaldy@upe.br>
2025-03-23 16:51:14 -0300

    feat: add methods to create and send requests in DataDragonApiService

--- src/main/java/br/com/devgui/service/DataDragonApiService.java
+++ src/main/java/br/com/devgui/service/DataDragonApiService.java
@@ -1,5 +1,28 @@
 package br.com.devgui.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class DataDragonApiService {
 
+	private final String URL_DATA_CHAMPION = "https://ddragon.leagueoflegends.com/cdn/15.2.1/data/pt_BR/champion.json";
+	
+	private final HttpClient client = HttpClient.newHttpClient();
+	
+	private HttpRequest createRequest(String url) {
+		return HttpRequest.newBuilder()
+				.uri(URI.create(url))
+				.GET()
+				.build();
+	}
+	
+	private HttpResponse<String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
+		return client.send(request, HttpResponse.BodyHandlers.ofString());
+	}
+	
+	
+	
 }
