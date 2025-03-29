0dd705306c7ac091f35de9d15b695429fe89f1b9
Guilherme Rivaldy <guilherme.rivaldy@upe.br>
2025-03-29 10:10:11 -0300

    feat: implement database connection

---
+++ src/main/java/br/com/devgui/config/DataBaseConnection.java
@@ -0,0 +1,45 @@
+package br.com.devgui.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+
+  private static final String URL = "jdbc:postgresql://localhost:5432/servlet-riot-1";
+  private static final String USER = "postgres";
+  private static final String PASSWORD = "prog123";
+  private final Connection connection;
+
+  public DataBaseConnection() {
+    this.connection = this.openConnection();
+  }
+
+
+  public Connection openConnection() {
+    try {
+
+      Class.forName("org.postgresql.Driver");
+      return DriverManager.getConnection(URL, USER, PASSWORD);
+
+    } catch (SQLException e) {
+      System.err.println("Error connecting to the database: " + e.getMessage());
+      return null;
+
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(
+          "PostgreSQL JDBC Driver not found. Make sure the driver JAR is in the classpath.");
+    }
+  }
+
+  public void closeConnection() {
+    if (connection != null) {
+      try {
+        connection.close();
+      } catch (SQLException e) {
+        throw new RuntimeException("Error while closing connection.");
+      }
+    }
+  }
+
+}
